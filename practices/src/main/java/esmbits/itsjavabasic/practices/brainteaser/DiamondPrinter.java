aa2c141c2e9a5d997f1957b3bbbd3e1e566fa2e6
kyamadahoge <k-yamada@esm.co.jp>
2017-11-02 13:46:57 +0900

    ~最初の課題

---
+++ practices/src/main/java/esmbits/itsjavabasic/practices/brainteaser/DiamondPrinter.java
@@ -0,0 +1,22 @@
+package esmbits.itsjavabasic.practices.brainteaser;
+
+public class DiamondPrinter {
+
+    public static void main(String... args) {
+
+        // コンソールにひし形(◆)を出力する.
+        // 大きさは任意に変えられるようにする.
+
+        // 大きさ:2
+        //  *
+        // ***
+        //  *
+        //
+        // 大きさ:3
+        //   *
+        //  ***
+        // *****
+        //  ***
+        //   *
+    }
+}
