791017771863a806ef507262e7eddcd91b68ce63
mahaker <hideaki.matsunami@esm.co.jp>
2017-11-21 14:56:16 +0900

    正方形が出力できるようになった。

--- practices/src/main/java/esmbits/itsjavabasic/practices/brainteaser/DiamondPrinter.java
+++ practices/src/main/java/esmbits/itsjavabasic/practices/brainteaser/DiamondPrinter.java
@@ -1,6 +1,15 @@
 package esmbits.itsjavabasic.practices.brainteaser;
 
 public class DiamondPrinter {
+    
+    private static String getRow(int size, int maxWidth ,int current) {
+        StringBuilder row = new StringBuilder("");
+//        int range = size - current;
+        for(int i = 0; i < maxWidth; i++) {
+            row.append("*");
+        }
+        return row.append("\n").toString();
+    }
 
     public static void main(String... args) {
 
@@ -18,5 +27,28 @@ public class DiamondPrinter {
         // *****
         //  ***
         //   *
+        // 大きさ:4
+        //     *
+        //    ***
+        //   *****
+        //  *******
+        // *********
+        //  *******
+        //   *****
+        //    ***
+        //     *
+        int size = Integer.parseInt(args[0]);
+        if(size < 2) throw new IndexOutOfBoundsException("sizeには2以上の数値を指定してください。");
+
+        int maxWidth = 2 * size - 1;
+        int maxHeight = 2 * size - 1;
+        
+        StringBuilder result = new StringBuilder("");
+        
+        for(int i = 0; i < maxHeight; i++) {
+          result.append( getRow(size, maxWidth, i) );
+        }
+        
+        System.out.println(result);
     }
 }
