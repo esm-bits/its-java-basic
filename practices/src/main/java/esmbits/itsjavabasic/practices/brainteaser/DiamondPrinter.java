b5dd883c7e2b0f64be025aeaab6fd3beb91e426d
m-miyagawa <miyagawa@esm.co.jp>
2017-12-04 14:30:40 +0900

    diamond print push

--- practices/src/main/java/esmbits/itsjavabasic/practices/brainteaser/DiamondPrinter.java
+++ practices/src/main/java/esmbits/itsjavabasic/practices/brainteaser/DiamondPrinter.java
@@ -2,7 +2,7 @@ package esmbits.itsjavabasic.practices.brainteaser;
 
 public class DiamondPrinter {
 
-    public static void main(String... args) {
+    public static void main(String[] args) {
 
         // コンソールにひし形(◆)を出力する.
         // 大きさは任意に変えられるようにする.
@@ -18,5 +18,24 @@ public class DiamondPrinter {
         // *****
         //  ***
         //   *
+    	int countX;
+    	int countY;
+    	int size = Integer.parseInt(args[0]);
+    	String linePrint = new String();
+
+    	// 中心点から距離で出力の判定を行う
+    	for (countY = 1; countY <= size * 2 - 1; countY++) {
+        	for (countX = 1; countX <= size * 2 - 1; countX++) {
+        		if (Math.abs(countX - size) + Math.abs(countY - size) <= size - 1) {
+           			linePrint = linePrint + "*";
+           		} else {
+           			linePrint = linePrint + " ";
+        		}
+        	}
+
+    		System.out.println(linePrint);
+    		linePrint = new String();
+    	}
+
     }
 }
