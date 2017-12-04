22fb7e01581fda231daac82c302047fd8f3f4c8a
esm-tkato <tkato@esm.co.jp>
2017-12-04 14:20:32 +0900

    ひし形を出力する処理を実装

--- practices/src/main/java/esmbits/itsjavabasic/practices/brainteaser/DiamondPrinter.java
+++ practices/src/main/java/esmbits/itsjavabasic/practices/brainteaser/DiamondPrinter.java
@@ -18,5 +18,62 @@ public class DiamondPrinter {
         // *****
         //  ***
         //   *
+        
+        int stageCount = Integer.parseInt(args[0]);
+        if(stageCount < 2) {
+            java.lang.System.out.println("引数には、2以上の整数を指定ください。");
+            System.exit(-1);
+        }
+        
+        Diamond diamond = new Diamond();
+        diamond.editDiamond(stageCount);
+    }
+    
+    static class Diamond {
+        Diamond() {
+            
+        }
+        
+        public void editDiamond(int a) {
+
+            int n = calcLengthOneSide(a);
+            String s = new String("");
+
+            for (int i = 0; i < n; i++) {
+                //出力文字列を初期化
+                s = "";
+                for (int j = 0; j < n; j++) {
+                    //空白の数を算出する
+                    int spaceCount = Math.abs((a - 1) - i);
+                    
+                    if(j < spaceCount) {
+                        //前半の空白を出力する範囲
+                        s = s + Constants.KINDS_OF_CHAR.SPACE;
+                    } else if(j >= spaceCount && j < n - spaceCount) {
+                        //描画文字を出力する範囲
+                        s = s + Constants.KINDS_OF_CHAR.DRAW_CHAR;
+                    } else {
+                        //後半の空白を出力する範囲
+                        s = s + Constants.KINDS_OF_CHAR.SPACE;
+                    }
+                }
+                java.lang.System.out.println(s);
+            }
+        }
+        
+        // 図形の幅(高さ)を求める
+        public int calcLengthOneSide(int b) {
+            return 2*b-1;
+        }        
+    }
+    
+    //定数定義
+    public static final class Constants {
+        private Constants() {
+        }
+        public static class KINDS_OF_CHAR {
+            public static final String SPACE = " ";
+            public static final String DRAW_CHAR = "*";
+        }
     }
 }
