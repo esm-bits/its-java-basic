a6f9482e434044439c86b1bcc55d70445d01e5fc
tsuji-k <kota.tsuji@esm.co.jp>
2017-12-04 14:42:12 +0900

    DiamondPrinter実装

--- practices/src/main/java/esmbits/itsjavabasic/practices/brainteaser/DiamondPrinter.java
+++ practices/src/main/java/esmbits/itsjavabasic/practices/brainteaser/DiamondPrinter.java
@@ -18,5 +18,25 @@ public class DiamondPrinter {
         // *****
         //  ***
         //   *
+        int i = 4;
+        print(i);
+    }
+
+    private static void print(int size) {
+        String str = "○";
+        String space = "●";
+        int length = size * 2 - 1;
+
+        for(int i = 1; i <= length; i++) {
+            String row = "";
+            for(int j = 1; j <= length; j++) {
+                if(Math.abs(j - size) + Math.abs(i - size) < size) {
+                    row += str;
+                } else {
+                    row += space;
+                }
+            }
+            System.out.println(row);
+        }
     }
 }
