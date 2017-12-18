595c2ecadf051c3c95da5a561e60702c4e76b0fe
kmikami <kmikami@esm.co.jp>
2017-12-18 14:34:08 +0900

    課題：ひし形

--- practices/src/main/java/esmbits/itsjavabasic/practices/brainteaser/DiamondPrinter.java
+++ practices/src/main/java/esmbits/itsjavabasic/practices/brainteaser/DiamondPrinter.java
@@ -2,6 +2,28 @@ package esmbits.itsjavabasic.practices.brainteaser;
 
 public class DiamondPrinter {
 
+
+	private void printDia(int n) {
+		int max = (n % 2) == 0 ? n+1 : n; // 最大幅
+		int spaceCnt = 0; // 半角スペースの数
+		int end = max / 2; // 終了条件
+
+		for (int i = 0; i < max; i++) {
+			if (i <= max / 2) { // 半分から上
+				spaceCnt = (max / 2) - i;
+				end++;
+			} else { // 半分より下
+				spaceCnt = i - (max / 2);
+				end--;
+			}
+
+			for (int j = 0; j < end; j++) {
+				System.out.print(j < spaceCnt ? " " : "*");
+			}
+			System.out.println();
+		}
+	}
+
     public static void main(String... args) {
 
         // コンソールにひし形(◆)を出力する.
@@ -18,5 +40,15 @@ public class DiamondPrinter {
         // *****
         //  ***
         //   *
+//    	System.out.println(" *");
+//    	System.out.println("***");
+//    	System.out.println(" *");
+
+    	int n = 1;
+    	if (args.length != 0) {
+    	  n = Integer.parseInt(args[0]);
+    	}
+
+    	new DiamondPrinter().printDia(n);
     }
 }
