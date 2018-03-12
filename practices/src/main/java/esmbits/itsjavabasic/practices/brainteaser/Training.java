01ab46b6d5d4db6563bb39f7195aa8b04b3374ad
kyamadahoge <k-yamada@esm.co.jp>
2018-03-12 13:25:08 +0900

    問題を追加

---
+++ practices/src/main/java/esmbits/itsjavabasic/practices/brainteaser/Training.java
@@ -0,0 +1,7 @@
+package esmbits.itsjavabasic.practices.brainteaser;
+
+public class Training {
+
+    // 和が value となるような連続する 2 つ以上の正の整数の組み合わせを出力する.
+    // (例) value が 9 の場合は 2+3+4 と 4+5 の二通りがある.
+}
