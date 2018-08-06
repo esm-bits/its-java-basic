4ca2d1c17ec6568bdf9d4a138201c8f85fc44047
kyamadahoge <k-yamada@esm.co.jp>
2018-08-06 14:25:11 +0900

    MyListの課題作成

---
+++ practices/src/main/java/esmbits/itsjavabasic/practices/abstracts/MyList.java
@@ -0,0 +1,21 @@
+package esmbits.itsjavabasic.practices.abstracts;
+
+public interface MyList {
+
+    // (1) 以下のメソッドを持つMyListインタフェースを作って下さい
+    //    1. add(String)
+    //      リストの最後に指定された文字列を追加する
+    //    2. get(int)
+    //      指定された位置にある文字列を返す
+    //    3. remove(int)
+    //      指定された位置にある文字列を削除し、その文字列を返す
+    //    4. size()
+    //      リスト内にある文字列の数を返す
+    //
+    // (2) MyListインタフェースを実装する
+    //     MyArrayListとMyLinkedListを実装して下さい
+    //
+    // (3) 2つの実装クラスを両方ともMyListとして扱う任意のコードを書いて下さい
+    //     例えば、2つの実装クラス間の性能の違いを確認する、など
+
+}
