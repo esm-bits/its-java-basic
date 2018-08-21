a13143c76d066d54db98faa3b6a5d91ab7c18320
reisato <reika.sato@esm.co.jp>
2018-08-21 15:05:41 +0900

    addを実装した。テストが途中

--- practices/src/main/java/esmbits/itsjavabasic/practices/abstracts/MyArrayList.java
+++ practices/src/main/java/esmbits/itsjavabasic/practices/abstracts/MyArrayList.java
@@ -2,4 +2,44 @@ package esmbits.itsjavabasic.practices.abstracts;
 
 public class MyArrayList implements MyList {
 
+	private String[] strArray = null;
+
+	private int index = 0;
+
+	public MyArrayList() {
+		this.strArray = new String[5];
+	}
+
+	@Override
+	public void add(String str) {
+		// TODO 自動生成されたメソッド・スタブ
+		if(index > strArray.length) {
+			String[] cloneArray = strArray.clone();
+			strArray = new String[strArray.length + 5];
+			for(int i = 0; i < cloneArray.length; i++) {
+				strArray[i] = cloneArray[i];
+			}
+		}
+		strArray[index] = str;
+		index++;
+	}
+
+	@Override
+	public String get(int number) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public String remove(int number) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
+	}
+
 }
---
+++ practices/src/test/java/esmbits/itsjavabasic/practices/abstracts/MyArrayListTest.java
@@ -0,0 +1,15 @@
+package esmbits.itsjavabasic.practices.abstracts;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyArrayListTest {
+
+	@Test
+	public void testAdd() {
+		MyList myList = new MyArrayList();
+		fail("まだ実装されていません");
+	}
+
+}
