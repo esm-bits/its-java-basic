4a95a3cf2783f4db54a1cc0e70a6317f74b20204
tToyoshima <t-toyoshima@esm.co.jp>
2018-10-01 15:02:28 +0900

    Sizeのテストを追記 確保済み配列にデータ入れていないときに例外発生する実装とテスト

--- practices/src/main/java/esmbits/itsjavabasic/practices/abstracts/MyArrayList.java
+++ practices/src/main/java/esmbits/itsjavabasic/practices/abstracts/MyArrayList.java
@@ -10,14 +10,16 @@ public class MyArrayList implements MyList {
 	private int index = 0;
 
 	public MyArrayList() {
-	    // TODO Addしていない配列に空文字が入る？
 		this.strArray = new String[5];
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void add(String str) {
 	    // インデクスが配列要素より大きい場合は、要素を5増やして置き換え
-		if(index > strArray.length) {
+		if(index >= strArray.length) {
 			String[] cloneArray = strArray.clone();
 			strArray = new String[strArray.length + 5];
 			for(int i = 0; i < cloneArray.length; i++) {
@@ -28,15 +30,17 @@ public class MyArrayList implements MyList {
 		index++;
 	}
 
+	/**
+     * {@inheritDoc}
+     */
 	@Override
 	public String get(int number) {
-	    //TODO
 
 	    //指定された要素番号のデータを取得
 	    String bufString = null;
 
 	    // 要素の数より大きな番号が指定された場合、例外を送出
-	    if (number > index) {
+	    if (number >= index) {
 	        throw new IndexOutOfBoundsException();
         }
 
@@ -46,16 +50,21 @@ public class MyArrayList implements MyList {
 	    return bufString;
 	}
 
+	/**
+     * {@inheritDoc}
+     */
 	@Override
 	public String remove(int number) {
 		// TODO 自動生成されたメソッド・スタブ
 		return null;
 	}
 
+	/**
+     * {@inheritDoc}
+     */
 	@Override
 	public int size() {
-		// TODO 自動生成されたメソッド・スタブ
-		return 0;
+		return index;
 	}
 
 }
--- practices/src/test/java/esmbits/itsjavabasic/practices/abstracts/MyArrayListTest.java
+++ practices/src/test/java/esmbits/itsjavabasic/practices/abstracts/MyArrayListTest.java
@@ -4,8 +4,11 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
+
 import com.sun.tracing.dtrace.ArgsAttributes;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class MyArrayListTest {
 
@@ -18,20 +21,19 @@ public class MyArrayListTest {
      * add()のテスト.
      */
 	@Test
-	public void test() {
+	public void test_add_インデクスが配列要素より小さい() {
 		//MyListを作成
 	    MyList myList = new MyArrayList();
 		myList.add("test1");
 		myList.add("test2");
 		myList.add("test3");
 		myList.add("test4");
-		myList.add("test5");
 
 		//要素が５個入っているか確認
-		assertThat(myList.size(), is(5));
+		assertThat(myList.size(), is(4));
 
 		//３つ目の要素を取得
-		// assertThat(myList.get(2), is("test3"));
+		 assertThat(myList.get(2), is("test3"));
 	}
 
     /**
@@ -51,8 +53,41 @@ public class MyArrayListTest {
 
         assertThat(myList.size(), is(6));
         assertThat(myList.get(5), is("test6"));
-        thrown.expect(IndexOutOfBoundsException.class);
-        myList.get(6);
+    }
+
+    /**
+     * add()のテスト.
+     */
+    @Test
+    public void test_add_配列に設定していない場合はエラーにする() {
+        //MyListを作成
+        MyList myList = new MyArrayList();
+        myList.add("test1");
+
+        //要素が５個入っているか確認
+        assertThat(myList.size(), is(1));
+
+        //３つ目の要素を取得
+        try {
+            myList.get(1);
+            fail();
+        } catch (IndexOutOfBoundsException e) {
+
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    /**
+     * size()のテスト.
+     */
+    @Test
+    public void test_size_サイズがゼロ() {
+        //MyListを作成
+        MyList myList = new MyArrayList();
+
+        //要素が0個入っているか確認
+        assertThat(myList.size(), is(0));
     }
 
 }
